dd548e1b0df5faefc9f39ae935aaa20de307d74b
thisispatt <patrick@geoffroy.fr>
2026-07-10 11:31:53 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -318,9 +318,9 @@ export const knockout = {
     // r16-3 & r16-4, qf-3 by r16-5 & r16-6, qf-4 by r16-7 & r16-8. When filling
     // in teams, place them by bracket position here, not by kickoff date.
     { id: "qf-1", home: "France", away: "Morocco", score1: 2, score2: 0, pens1: null, pens2: null, date: "9 Jul",  time: "21:00" },
-    { id: "qf-2", home: null, away: null, score1: null, score2: null, pens1: null, pens2: null, date: "10 Jul", time: "20:00" },
-    { id: "qf-3", home: null, away: null, score1: null, score2: null, pens1: null, pens2: null, date: "11 Jul", time: "22:00" },
-    { id: "qf-4", home: null, away: null, score1: null, score2: null, pens1: null, pens2: null, date: "12 Jul", time: "02:00" },
+    { id: "qf-2", home: "Spain", away: "Belgium", score1: null, score2: null, pens1: null, pens2: null, date: "10 Jul", time: "20:00" },
+    { id: "qf-3", home: "Norway", away: "England", score1: null, score2: null, pens1: null, pens2: null, date: "11 Jul", time: "22:00" },
+    { id: "qf-4", home: "Argentina", away: "Switzerland", score1: null, score2: null, pens1: null, pens2: null, date: "12 Jul", time: "02:00" },
   ],
   SF: [
     // sf-1 fed by qf-1 & qf-2, sf-2 fed by qf-3 & qf-4 — bracket position, not date.
